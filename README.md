d58ef4166ddc5b6f26eef4bbf0795639a6d65a0b
AlexanderMarinov894 <alexander.mrnv@gmail.com>
2014-11-15 00:14:55 +0200

    -uploaded idea for game layout

--- README.md
+++
@@ -1,2 +0,0 @@
-JavaScript-Team-TAMARILLO
-=========================
---
+++ js/main.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById('mainGame');
+var ctx = canvas.getContext('2d');
+var W = canvas.width;
+var H = canvas.height;
